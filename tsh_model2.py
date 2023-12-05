24e6df31e0eceb1756f703fea5bb494e7a26acd3
Nicole Goebel <nicolegoebel@gmail.com>
2023-12-05 12:41:19 -0800

    fix something add in exact dose

--- tsh_model2.py
+++ tsh_model2.py
@@ -147,8 +147,9 @@ else:
     high_tsh = 4.2
 TSH_initial_high = int(TSH1 > high_tsh)
 TSH_initial_normal = int(low_tsh<=TSH1<=high_tsh)
+
 exact_dose = np.round(predict_new_dose(model, weight, initial_weekly_dose, TSH1, TSH_initial_high, TSH_initial_normal), 1)
-if TSH1>=tsh_high and new_dose <= initial_weekly_dose:  # TSH is too high and new_dose is less than or equL to initial weekly dose
+if TSH1>=high_tsh and new_dose <= initial_weekly_dose:  # TSH is too high and new_dose is less than or equL to initial weekly dose
     exact_dose += increase_increment
 elif high_tsh >= TSH1 >= low_tsh:    # normal
     #if health == "well":
@@ -159,4 +160,4 @@ elif high_tsh >= TSH1 >= low_tsh:    # normal
 #new_dose = False
 st.session_state["new_dose"] = math.floor(exact_dose / 25) * 25
 delta=round(st.session_state["new_dose"]-initial_weekly_dose, 1)
-st.metric("Your new dose is:", value=f'{st.session_state["new_dose"]} mL', delta=f'{delta} mL')
+st.metric("Your new dose is:", value=f'{st.session_state["new_dose"]} mL', delta=f'{delta} mL', exact_dose=f'{exact_dose} mL')
